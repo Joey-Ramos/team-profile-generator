2bb249bcf7e7e527a52261456ef708c2190a905c
Joey Ramos <jramos1202@gmail.com>
2020-11-08 19:31:05 -0600

    pushing changes to git

---
+++ __tests__/Mananger.test.js
@@ -0,0 +1,34 @@
+const Manager = require('../lib/Manager');
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// check that super constructor properties are extant and of correct type
+test('check properties inherited from Employee', () => {
+	const manager = new Manager('John Smith', 1, 'john@email.com', 123);
+
+	expect(manager).toHaveProperty('name', expect.any(String));
+	expect(manager).toHaveProperty('id', expect.any(Number));
+	expect(manager).toHaveProperty('email', expect.stringMatching(emailRegEx));
+});
+
+// check officeNumber property added in own constructor
+test('check manager has office number', () => {
+	const manager = new Manager('John Smith', 1, 'john@email.com', 123);
+
+	expect(manager).toHaveProperty('officeNumber', expect.any(Number));
+});
+
+// check that manager.getOfficeNumber() returns office number output string
+test("outputs string with a manager's office number", () => {
+	const manager = new Manager('John Smith', 1, 'john@email.com', 123);
+
+	expect(manager.getOfficeNumber()).toEqual(
+		expect.stringContaining(manager.officeNumber.toString())
+	);
+});
+
+// check that manager.getRole() returns role output string
+test("outputs string with manager's role", () => {
+	const manager = new Manager('John Smith', 1, 'john@email.com', 123);
+
+	expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+});
